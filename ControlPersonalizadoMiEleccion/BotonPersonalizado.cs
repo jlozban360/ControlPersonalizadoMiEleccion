40de67af4e18b1c998f4d516d9656b4f9468d64e
jlozban360 <jlozban360@g.educaand.es>
2024-11-27 13:40:50 +0100

    Agregar archivos de proyecto.

---
+++ ControlPersonalizadoMiEleccion/BotonPersonalizado.cs
@@ -0,0 +1,22 @@
+﻿namespace ControlPersonalizadoMiEleccion
+{
+    public partial class BotonPersonalizado : Button
+    {
+        public BotonPersonalizado()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+
+            Random rand = new Random();
+            int r = rand.Next(256);
+            int g = rand.Next(256);
+            int b = rand.Next(256);
+
+            this.BackColor = Color.FromArgb(r, g, b);
+        }
+    }
+}
